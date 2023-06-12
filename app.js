7b64559beab179ba3cb32deb691e5b64e1598796
Marcelo Honório dos Santos <marcelohonsa@gmail.com>
2023-06-11 22:59:30 -0400

    fix

--- app.js
+++ app.js
@@ -2,30 +2,32 @@ const { Blockchain } = require('./Blockchain.js');
 const { Block } = require('./Block.js');
 
 
-let tbCoin = new Blockchain();
+var tbCoin = new Blockchain();
 console.log("tbCoin mining in progress....");
 console.log('Adding Chain 1...');
 tbCoin.addChain(
   new Block(1, "11/06/2023", {
-    sender: "Bella Costa Honório dos Santos",
-    recipient: "Pietra Costa Honório dos Santos",
-    quantity: 50
+    origin: "Bella Costa Honório dos Santos",
+    destiny: "Pietra Costa Honório dos Santos",
+    amount: 50
   })
 );
+
 console.log('Adding Chain 2...');
 tbCoin.addChain(
   new Block(2, "11/06/2023", {
-    sender: "Pietra Costa Honório dos Santos",
-    recipient: "Barbara Vitor Costa Honório dos Santos",
-    quantity: 100
+    origin: "Pietra Costa Honório dos Santos",
+    destiny: "Barbara Vitor Costa Honório dos Santos",
+    amount: 100
   })
 );
+
 console.log('Adding Chain 3...');
 tbCoin.addChain(
     new Block(3, "11/06/2023", {
-      sender: "Barbara Vitor Costa Honório dos Santos",
-      recipient: "Marcelo Honório dos Santos",
-      quantity: 350
+      origin: "Barbara Vitor Costa Honório dos Santos",
+      destiny: "Marcelo Honório dos Santos",
+      amount: 350
     })
   );
 
